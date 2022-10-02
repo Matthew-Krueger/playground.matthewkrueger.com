92097b5c115de886a9878079b94399a154e7c816
Matthew Krueger <contact@matthewkrueger.com>
2022-10-02 16:52:58 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url='https://www.matthewkrueger.com'" />
+  </head>
+  <body>
+    <p>Please follow <a href="https://www.matthewkrueger.com">this link</a>. This site is only to host specific projects made by Matthew Krueger. As such,
+    it has no content unless linked by my main page. Thanks!</p>
+  </body>
+</html>
