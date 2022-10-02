ee2e79c0d040b42847457647b8aa92e6bfa3e932
Matthew Krueger <contact@matthewkrueger.com>
2022-10-02 14:25:51 -0400

    Update git-pull.php

--- git-pull.php
+++ git-pull.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
 require_once("../common/playground-common.php");
 
 define("REMOTE_REPOSITORY", "git@github.com:Matthew-Krueger/playground.matthewkrueger.com.git"); // The SSH URL to your repository
