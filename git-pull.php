90d0f2a938684fa93521720a6527a886080fb06d
Matthew Krueger <contact@matthewkrueger.com>
2022-10-02 15:23:32 -0400

    Update git-pull.php

--- git-pull.php
+++ git-pull.php
@@ -18,6 +18,7 @@ define("BEFORE_PULL", "");                                             // A comm
 define("AFTER_PULL", "");        
 
 $content = file_get_contents("php://input");
+echo $content;
 $json    = json_decode($content, true);
 $file    = fopen(LOGFILE, "a");
 $time    = time();
